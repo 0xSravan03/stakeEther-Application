bbcc677af3b27e3aca984dbfdb546d7516eac959
Sravan T P <sravantp03@gmail.com>
2023-05-10 15:13:38 +0530

    completed test for staking contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     localhost: {},
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   solidity: "0.8.18",
 };
