30d242e2ef2953fa3f5c0b9a87db11799dec0b59
Sravan T P <sravantp03@gmail.com>
2023-05-10 15:30:47 +0530

    created deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,15 @@
 const { ethers } = require("hardhat");
 
-async function main() {}
+async function main() {
+  const [signer] = await ethers.getSigners();
+  console.log("Deploying contract...");
+  const StakingFactory = await ethers.getContractFactory("Staking", signer);
+  const Staking = await StakingFactory.deploy({
+    value: ethers.utils.parseEther("10"),
+  });
+  await Staking.deployed();
+  console.log(`Contract Deployed at : ${Staking.address} by ${signer.address}`);
+}
 
 main().catch((error) => {
   console.error(error);
